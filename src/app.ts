65fd13a01e6f539e99bba0fb7db9148b6a3e8d64
Yu Yongwoo <me@yowu.dev>
2020-04-27 16:52:19 +0900

    newrelic 이 최상위에서 호출되도록 수정

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import debug from 'debug';
+import http from 'http';
+
+import app from './server';
+
+const log = debug('trv:index');
+
+export function bootstrap() {
+  const server = http.createServer(app);
+
+  let currentApp = app;
+
+  const port = process.env.PORT || 3000;
+  server.listen(port, (error: any) => {
+    if (error) {
+      log(error);
+    }
+
+    log(`🚀 started on ${port}`);
+  });
+
+  if (module.hot) {
+    log('✅  Server-side HMR Enabled!');
+
+    module.hot.accept('./server', () => {
+      log('🔁  HMR Reloading `./server`...');
+      server.removeListener('request', currentApp);
+      const newApp = require('./server').default;
+      server.on('request', newApp);
+      currentApp = newApp;
+    });
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,11 @@
 import 'dotenv/config';
 
+import { bootstrap } from './app';
+
 if (process.env.RUN_ENV === 'server') {
   // tslint:disable-next-line: no-var-requires
   require('newrelic');
   console.log('RUN_ENV is "server", so execute Newrelic agent.');
 }
 
-import debug from 'debug';
-import http from 'http';
-
-import app from './server';
-
-const log = debug('trv:index');
-
-const server = http.createServer(app);
-
-let currentApp = app;
-
-const port = process.env.PORT || 3000;
-server.listen(port, (error: any) => {
-  if (error) {
-    log(error);
-  }
-
-  log(`🚀 started on ${port}`);
-});
-
-if (module.hot) {
-  log('✅  Server-side HMR Enabled!');
-
-  module.hot.accept('./server', () => {
-    log('🔁  HMR Reloading `./server`...');
-    server.removeListener('request', currentApp);
-    const newApp = require('./server').default;
-    server.on('request', newApp);
-    currentApp = newApp;
-  });
-}
+bootstrap();
